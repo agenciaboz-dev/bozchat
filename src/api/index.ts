e99c51aceb69402973814a0d1486db498b5c4185
nandobfer <nandobfer@yahoo.com.br>
2024-11-04 17:43:42 -0300

    update: removed timeout

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,6 @@ import axios from "axios"
 import { url } from "./backend"
 
 export const api = axios.create({
-    // baseURL: "http://localhost:4105/api",
     baseURL: `http${url}`,
-    timeout: 1000 * 20,
+    // timeout: 1000 * 20,
 })
