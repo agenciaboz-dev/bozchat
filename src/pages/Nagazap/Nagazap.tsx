696069d38820e29336d0671de2c46c330c534c04
nandobfer <nandobfer@yahoo.com.br>
2024-11-05 17:14:27 -0300

    update: nagazap header

--- src/pages/Nagazap/Nagazap.tsx
+++ src/pages/Nagazap/Nagazap.tsx
@@ -15,11 +15,11 @@ import { Blacklist } from "./Blacklist/Blacklist"
 import { useIo } from "../../hooks/useIo"
 import { Logs } from "./Logs/Logs"
 import { Title } from "../../components/Title"
+import { Header } from "../../components/Header"
 
-interface NagazapProps {
-}
+interface NagazapProps {}
 
-export const NagazapScreen: React.FC<NagazapProps> = ({  }) => {
+export const NagazapScreen: React.FC<NagazapProps> = ({}) => {
     const io = useIo()
 
     const [nagazap, setNagazap] = useState<Nagazap>()
@@ -50,47 +50,50 @@ export const NagazapScreen: React.FC<NagazapProps> = ({  }) => {
     }, [])
 
     return (
-        <Box sx={{ ...backgroundStyle, padding: "2vw", paddingBottom: "8vw", flexDirection: "row" }}>
-            <Paper
-                sx={{
-                    width: "17vw",
-                    height: "100%",
-                    bgcolor: "background.paper",
-                    flexDirection: "column",
-                    overflowY: "auto",
-                    padding: "1vw 0",
-                    gap: "1.5vw",
-                    borderRadius: "0 0 0 2vw ",
-                    color: "secondary.main",
-                    overflowX: "hidden",
-                    // hiding scrollbar
-                    "&::-webkit-scrollbar": {
-                        width: 0,
-                        height: 0,
-                    },
-                }}
-                elevation={5}
-            >
-                <Title title="Nagazap" icon={<WhatsApp />}>
-                    <ToolButton label="Informações" route="/" />
-                    <ToolButton label="Mensagens" route="/messages" />
-                    <ToolButton label="Enviar mensagem" route="/message_form" />
-                    <ToolButton label="Forno" route="/oven" />
-                    <ToolButton label="Logs" route="/logs" />
-                    <ToolButton label="Lista negra" route="/blacklist" />
-                    <ToolButton label="Token" route="/token" />
-                </Title>
-            </Paper>
-            <Box sx={{ width: "80vw" }}>
-                <Routes>
-                    <Route index element={<Info />} />
-                    <Route path="/token" element={<Token nagazap={nagazap} setNagazap={setNagazap} />} />
-                    <Route path="/messages" element={<MessagesScreen />} />
-                    <Route path="/oven" element={<Oven nagazap={nagazap} setNagazap={setNagazap} />} />
-                    <Route path="/blacklist" element={<Blacklist nagazap={nagazap} setNagazap={setNagazap} />} />
-                    <Route path="/logs" element={<Logs nagazap={nagazap} setNagazap={setNagazap} />} />
-                    <Route path="/message_form" element={<MessageFormScreen />} />
-                </Routes>
+        <Box sx={{ ...backgroundStyle }}>
+            <Header />
+            <Box sx={{ padding: "2vw", flex: 1 }}>
+                <Paper
+                    sx={{
+                        width: "17vw",
+                        height: "100%",
+                        bgcolor: "background.paper",
+                        flexDirection: "column",
+                        overflowY: "auto",
+                        padding: "1vw 0",
+                        gap: "1.5vw",
+                        borderRadius: "0 0 0 2vw ",
+                        color: "secondary.main",
+                        overflowX: "hidden",
+                        // hiding scrollbar
+                        "&::-webkit-scrollbar": {
+                            width: 0,
+                            height: 0,
+                        },
+                    }}
+                    elevation={5}
+                >
+                    <Title title="Nagazap" icon={<WhatsApp />}>
+                        <ToolButton label="Informações" route="/" />
+                        <ToolButton label="Mensagens" route="/messages" />
+                        <ToolButton label="Enviar mensagem" route="/message_form" />
+                        <ToolButton label="Forno" route="/oven" />
+                        <ToolButton label="Logs" route="/logs" />
+                        <ToolButton label="Lista negra" route="/blacklist" />
+                        <ToolButton label="Token" route="/token" />
+                    </Title>
+                </Paper>
+                <Box sx={{ width: "80vw" }}>
+                    <Routes>
+                        <Route index element={<Info />} />
+                        <Route path="/token" element={<Token nagazap={nagazap} setNagazap={setNagazap} />} />
+                        <Route path="/messages" element={<MessagesScreen />} />
+                        <Route path="/oven" element={<Oven nagazap={nagazap} setNagazap={setNagazap} />} />
+                        <Route path="/blacklist" element={<Blacklist nagazap={nagazap} setNagazap={setNagazap} />} />
+                        <Route path="/logs" element={<Logs nagazap={nagazap} setNagazap={setNagazap} />} />
+                        <Route path="/message_form" element={<MessageFormScreen />} />
+                    </Routes>
+                </Box>
             </Box>
         </Box>
     )
