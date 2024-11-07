78e6ad27111e21b8d5587ae0c210527cc67e7d8f
alfredo-petri <alfredo_petri@hotmail.com>
2024-11-07 11:52:24 -0300

    feat: show document

--- src/pages/Washima/MediaInput/MediaListItem.tsx
+++ src/pages/Washima/MediaInput/MediaListItem.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { Box, IconButton, MenuItem, Paper } from "@mui/material"
+import { Avatar, Box, IconButton, MenuItem, Paper } from "@mui/material"
 import { Cancel, Close } from "@mui/icons-material"
+import { documentIcon } from "../../../tools/documentIcon"
 
 interface MediaListItemProps {
     file: File
@@ -11,11 +12,25 @@ interface MediaListItemProps {
 
 export const MediaListItem: React.FC<MediaListItemProps> = ({ file, is_current, onClick, onDelete }) => {
     const type = file.type.split("/")[0]
+    const documentType = file.name.split(".").pop()
     const url = file ? URL.createObjectURL(file) : ""
+    console.log(type)
     return (
         <MenuItem sx={{ padding: 0, width: "fit-content", position: "relative" }} onClick={onClick}>
             {type === "image" && <img src={url} style={{ width: "auto", height: "10vh", objectFit: "contain" }} />}
             {type === "video" && <video src={url} style={{ width: "auto", height: "10vh", objectFit: "contain" }} />}
+            {type !== "image" && type !== "video" && documentType && (
+                <Avatar
+                    sx={{
+                        width: "auto",
+                        height: "10vh",
+                        objectFit: "contain",
+                        borderRadius: 0,
+                    }}
+                    alt="icone"
+                    src={documentIcon(documentType)}
+                />
+            )}
             <IconButton
                 sx={{ position: "absolute", right: "0.1vw", top: "0.1vw", borderRadius: "100%", padding: 0 }}
                 color="warning"
