cd9e48e986a89b49843a3a16a5e0084aa32c69ef
ildopetrijunior <ildopetrijunior@gmail.com>
2024-11-08 13:57:59 -0300

    Update: date of message

--- src/pages/Zap/MessageDateContainer.tsx
+++ src/pages/Zap/MessageDateContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box } from "@mui/material"
+import { Box, useMediaQuery } from "@mui/material"
 import { MessageAck } from "./MessageAck"
 import { WashimaMessage } from "../../types/server/class/Washima/WashimaMessage"
 
@@ -11,18 +11,20 @@ interface MessageDateContainerProps {
 }
 
 export const MessageDateContainer: React.FC<MessageDateContainerProps> = ({ message, is_audio, is_image, is_document }) => {
+    const isMobile = useMediaQuery("(orientation: portrait)")
+
     return (
         <Box
             sx={{
                 position: is_audio || is_document ? "absolute" : undefined,
-                bottom: "0.5vw",
-                right: "0.5vw",
-                fontSize: "0.6vw",
+                bottom: isMobile ? "0.5vw" : "0.5vw",
+                right: isMobile ? "0.5vw" : "0.5vw",
+                fontSize: isMobile ? "3vw" : "0.6vw",
                 marginTop: is_audio ? "0.5vw" : undefined,
                 padding: is_image ? "0 0.25vw" : undefined,
                 justifyContent: "flex-end",
                 alignItems: "center",
-                gap: "0.2vw",
+                gap: isMobile ? "1vw" : "0.2vw",
             }}
         >
             {message.edited && <Box>Editado</Box>}
