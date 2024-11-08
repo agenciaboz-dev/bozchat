81d063a4dd34bb182643c388a54a0d701d6b079d
nandobfer <nandobfer@yahoo.com.br>
2024-11-08 10:54:20 -0300

    feat: added clear function on search

--- src/pages/Washima/WashimaSearch.tsx
+++ src/pages/Washima/WashimaSearch.tsx
@@ -1,37 +1,42 @@
-import React from "react"
-import { Box, TextField } from "@mui/material"
+import React, { useState } from "react"
+import { Box, IconButton, TextField } from "@mui/material"
 import { textFieldStyle } from "../../style/textfield"
-import { Search } from "@mui/icons-material"
+import { Close, Search } from "@mui/icons-material"
 import { string } from "yup"
 import { FormikErrors, FormikTouched } from "formik"
 
 interface WashimaSearchProps {
-    searchFormik: {
-        values: { washima_id: string; search: string }
-        initialValues: { washima_id: string; search: string }
-        errors: FormikErrors<{ washima_id: string; search: string }>
-        touched: FormikTouched<{ washima_id: string; search: string }>
-        handleChange: (e: React.ChangeEvent<any>) => void
-        handleBlur: {
-            (e: React.FocusEvent<any, Element>): void
-            <T = any>(fieldOrEvent: T): T extends string ? (e: any) => void : void
-        }
-        setFieldValue: (
-            field: string,
-            value: any,
-            shouldValidate?: boolean | undefined
-        ) => Promise<void> | Promise<FormikErrors<{ washima_id: string; search: string }>>
-    }
+    handleSearch: (value: string) => void
 }
 
-export const WashimaSearch: React.FC<WashimaSearchProps> = ({ searchFormik }) => {
+export const WashimaSearch: React.FC<WashimaSearchProps> = ({ handleSearch }) => {
+    const [value, setValue] = useState("")
+
+    const onChange = (text: string) => {
+        setValue(text)
+        handleSearch(text)
+    }
+
+    const clearSearch = () => {
+        setValue("")
+        handleSearch("")
+    }
+
     return (
         <TextField
             sx={textFieldStyle}
-            value={searchFormik.values.search}
-            onChange={(ev) => searchFormik.setFieldValue("search", ev.target.value)}
+            value={value}
+            onChange={(ev) => onChange(ev.target.value)}
             placeholder="Pesquisar chat ou mensagem"
-            InputProps={{ sx: { gap: "0.5vw" }, startAdornment: <Search /> }}
+            InputProps={{
+                sx: { gap: "0.5vw" },
+                startAdornment: <Search />,
+                endAdornment: (
+                    <IconButton sx={{ padding: 0 }} onClick={clearSearch}>
+                        <Close />
+                    </IconButton>
+                ),
+            }}
         />
     )
 }
