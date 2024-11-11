dd9bebf19bf2d940ab93338f67da22105fe0d570
ildopetrijunior <ildopetrijunior@gmail.com>
2024-11-11 15:42:04 -0300

    Update: skeleton of chatlist

--- src/pages/Zap/ChatSkeleton.tsx
+++ src/pages/Zap/ChatSkeleton.tsx
@@ -1,31 +1,39 @@
 import React from "react"
-import { Box, Skeleton } from "@mui/material"
+import { Box, Skeleton, useMediaQuery } from "@mui/material"
 
 interface ChatSkeletonProps {}
 
 export const ChatSkeleton: React.FC<ChatSkeletonProps> = ({}) => {
+    const isMobile = useMediaQuery("(orientation: portrait)")
     return (
         <Box
             sx={{
-                width: "100%",
-                height: "5vw",
-                borderRadius: "0.5vw",
+                width: isMobile ? "90vw" : "100%",
+                height: isMobile ? "20vw" : "5vw",
+                borderRadius: isMobile ? "3vw" : "0.5vw",
                 borderBottom: "2px solid",
-                padding: "1vw",
+                padding: isMobile ? "2vw 1vw" : "1vw",
                 alignItems: "center",
-                gap: "1vw",
-                flex: 1,
+                gap: isMobile ? "3vw" : "1vw",
+                // flex: 1,
             }}
         >
-            <Skeleton variant="circular" animation="wave" sx={{ width: "3vw", height: "3vw" }} />
+            <Skeleton variant="circular" animation="wave" sx={{ width: isMobile ? "15vw" : "3vw", height: isMobile ? "15vw" : "3vw" }} />
 
-            <Box sx={{ flexDirection: "column", justifyContent: "space-between", height: "100%", flex: 1 }}>
+            <Box
+                sx={{
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    height: "100%",
+                    flex: 1,
+                }}
+            >
                 <Box sx={{ justifyContent: "space-between", alignItems: "center", width: "100%" }}>
-                    <Skeleton variant="rounded" animation="wave" sx={{ width: "8vw" }} />
-                    <Skeleton variant="rounded" animation="wave" sx={{ width: "3vw", height: "0.6vw" }} />
+                    <Skeleton variant="rounded" animation="wave" sx={{ width: isMobile ? "50%" : "8vw" }} />
+                    <Skeleton variant="rounded" animation="wave" sx={{ width: isMobile ? "20%" : "3vw", height: isMobile ? "3vw" : "0.6vw" }} />
                 </Box>
 
-                <Skeleton variant="rounded" animation="wave" sx={{ width: "18vw" }} />
+                <Skeleton variant="rounded" animation="wave" sx={{ width: isMobile ? "85%" : "18vw" }} />
             </Box>
         </Box>
     )
