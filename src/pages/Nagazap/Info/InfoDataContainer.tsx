44e9b85812b061b8b8aab7fe13363f603fb2f2ef
ildopetrijunior <ildopetrijunior@gmail.com>
2024-11-11 19:44:16 -0300

    Update: Info component from mobile

--- src/pages/Nagazap/Info/InfoDataContainer.tsx
+++ src/pages/Nagazap/Info/InfoDataContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, IconButton, Skeleton } from "@mui/material"
+import { Box, IconButton, Skeleton, useMediaQuery } from "@mui/material"
 import { GeneralStat } from "../../../types/GeneralStat"
 import { CopyAll } from "@mui/icons-material"
 
@@ -8,16 +8,17 @@ interface InfoDataContainerProps {
 }
 
 export const InfoDataContainer: React.FC<InfoDataContainerProps> = ({ data }) => {
+    const isMobile = useMediaQuery("(orientation: portrait)")
     const Icon = data.icon
     return (
-        <Box sx={{}}>
+        <Box sx={{ border: "red solid 1px" }}>
             <Box sx={{ flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
                 <Box sx={{ gap: "0.5vw", alignItems: "center" }}>
-                    <Icon color="secondary" sx={{ width: "1vw", height: "1vw" }} />
+                    <Icon color="secondary" sx={{ width: isMobile ? "5vw" : "1vw", height: isMobile ? "5vw" : "1vw" }} />
                     <Box sx={[{ fontSize: "0.9rem", fontWeight: "bold", color: "secondary.main" }]}>{data.title}</Box>
                 </Box>
                 {data.loading ? (
-                    <Skeleton variant="rounded" animation="wave" sx={{ height: "2.21vw", width: "20vw" }} />
+                    <Skeleton variant="rounded" animation="wave" sx={{ height: isMobile ? "9vw" : "2.21vw", width: isMobile ? "80vw" : "20vw" }} />
                 ) : (
                     <Box sx={[{ fontSize: "1.5rem", fontWeight: "bold", color: "primary.main", gap: "0.5vw" }]}>
                         {data.value}
