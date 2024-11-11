b1e857f92fc41d9be40ae1eb2b7f20e5749798ab
ildopetrijunior <ildopetrijunior@gmail.com>
2024-11-11 19:16:22 -0300

    Update: child component calls

--- src/pages/Nagazap/Nagazap.tsx
+++ src/pages/Nagazap/Nagazap.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Box, MenuItem, Paper, TextField } from "@mui/material"
+import { Box, MenuItem, Paper, TextField, useMediaQuery } from "@mui/material"
 import { backgroundStyle } from "../../style/background"
 import { Add, AddCircle, Hub, WhatsApp } from "@mui/icons-material"
 import { ToolButton } from "./ToolButton"
@@ -29,10 +29,12 @@ export const NagazapScreen: React.FC<NagazapProps> = ({}) => {
     const io = useIo()
     const { user } = useUser()
     const navigate = useNavigate()
+    const isMobile = useMediaQuery("(orientation: portrait)")
 
     const [nagazapList, setNagazapList] = useState<Nagazap[]>([])
     const [nagazap, setNagazap] = useState<Nagazap>()
     const [loading, setLoading] = useState(false)
+    const [showInformations, setShowInformations] = useState(false)
 
     const fetchNagazap = async () => {
         if (!user) return
@@ -90,90 +92,118 @@ export const NagazapScreen: React.FC<NagazapProps> = ({}) => {
         <Box sx={{ ...backgroundStyle }}>
             <Header />
             <Box sx={{ padding: "2vw", flex: 1 }}>
-                <Paper
-                    sx={{
-                        width: "17vw",
-                        height: "100%",
-                        bgcolor: "background.paper",
-                        flexDirection: "column",
-                        overflowY: "auto",
-                        padding: "1vw 0",
-                        gap: "1.5vw",
-                        borderRadius: "0 0 0 2vw ",
-                        color: "secondary.main",
-                        overflowX: "hidden",
-                        // hiding scrollbar
-                        "&::-webkit-scrollbar": {
-                            width: 0,
-                            height: 0,
-                        },
-                    }}
-                    elevation={5}
-                >
-                    <Title title="Nagazap" icon={<Hub />}>
-                        <Box sx={{ flexDirection: "column", gap: "2vw" }}>
-                            <Box sx={{ padding: "0 2vw" }}>
-                                <TextField
-                                    sx={textFieldStyle}
-                                    select
-                                    value={nagazap?.id || ""}
-                                    label="Selecione uma conta"
-                                    SelectProps={{ MenuProps: { MenuListProps: { sx: { bgcolor: "background.default" } } } }}
-                                >
-                                    <MenuItem sx={{ fontWeight: "bold", gap: "1vw" }} onClick={onNewNagazapPress}>
-                                        <AddCircle />
-                                        Adicionar conta
-                                    </MenuItem>
-                                    {nagazapList.map((item) => (
-                                        <MenuItem key={item.id} value={item.id} onClick={() => onNagazapChoose(item)}>
-                                            {item.displayPhone} - {item.displayName}
+                {!showInformations ? (
+                    <Paper
+                        sx={{
+                            width: isMobile ? "100%" : "17vw",
+                            height: isMobile ? undefined : "100%",
+                            bgcolor: "background.paper",
+                            flexDirection: "column",
+                            overflowY: "auto",
+                            padding: isMobile ? "1vw" : "1vw 0",
+                            gap: "1.5vw",
+                            borderRadius: isMobile ? " 0 5vw 0 5vw" : "0 0 0 2vw ",
+                            color: "secondary.main",
+                            overflowX: "hidden",
+                            // hiding scrollbar
+                            "&::-webkit-scrollbar": {
+                                width: 0,
+                                height: 0,
+                            },
+                        }}
+                        elevation={5}
+                    >
+                        <Title title="Nagazap" icon={<Hub />}>
+                            <Box
+                                sx={{
+                                    flexDirection: "column",
+                                    gap: "2vw",
+                                }}
+                            >
+                                <Box sx={{ padding: isMobile ? "2vw" : "0 2vw", paddingBottom: isMobile ? 0 : undefined }}>
+                                    <TextField
+                                        sx={textFieldStyle}
+                                        select
+                                        value={nagazap?.id || ""}
+                                        label="Selecione uma conta"
+                                        SelectProps={{ MenuProps: { MenuListProps: { sx: { bgcolor: "background.default" } } } }}
+                                    >
+                                        <MenuItem sx={{ fontWeight: "bold", gap: "1vw" }} onClick={onNewNagazapPress}>
+                                            <AddCircle />
+                                            Adicionar conta
                                         </MenuItem>
-                                    ))}
-                                    {!nagazapList.length && <MenuItem disabled>Nenhuma conta encontrada</MenuItem>}
-                                </TextField>
-                            </Box>
-                            {nagazap && (
-                                <Box sx={{ flexDirection: "column" }}>
-                                    <ToolButton label="Informações" route="/" />
-                                    <ToolButton label="Mensagens" route="/messages" />
-                                    <ToolButton label="Criar template" route="/template-form" />
-                                    <ToolButton label="Enviar mensagem" route="/message-form" />
-                                    <ToolButton label="Forno" route="/oven" />
-                                    <ToolButton label="Logs" route="/logs" />
-                                    <ToolButton label="Lista negra" route="/blacklist" />
-                                    <ToolButton label="Configurações" route="/settings" />
+                                        {nagazapList.map((item) => (
+                                            <MenuItem key={item.id} value={item.id} onClick={() => onNagazapChoose(item)}>
+                                                {item.displayPhone} - {item.displayName}
+                                            </MenuItem>
+                                        ))}
+                                        {!nagazapList.length && <MenuItem disabled>Nenhuma conta encontrada</MenuItem>}
+                                    </TextField>
                                 </Box>
-                            )}
-                        </Box>
-                    </Title>
-                </Paper>
-                <Box sx={{ width: "80vw", overflowY: "auto", height: "100%" }}>
-                    {loading ? (
-                        <Box sx={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-                            <WagaLoading />
-                        </Box>
-                    ) : nagazap ? (
-                        <Routes>
-                            <Route index element={<Info nagazap={nagazap} />} />
-                            <Route path="/messages" element={<MessagesScreen nagazap={nagazap} />} />
-                            <Route
-                                path="/settings"
-                                element={<NagazapSettings nagazap={nagazap} setNagazap={setNagazap} fetchNagazaps={fetchNagazap} />}
-                            />
-                            <Route path="/oven" element={<Oven nagazap={nagazap} setNagazap={setNagazap} />} />
-                            <Route path="/blacklist" element={<Blacklist nagazap={nagazap} setNagazap={setNagazap} />} />
-                            <Route path="/logs" element={<Logs nagazap={nagazap} setNagazap={setNagazap} />} />
-                            <Route path="/template-form" element={<TemplateForm nagazap={nagazap} />} />
-                            <Route path="/message-form" element={<MessageFormScreen nagazap={nagazap} />} />
-                            <Route path="/form" element={<NagazapForm onSuccess={onAddNagazap} />} />
-                        </Routes>
-                    ) : (
-                        <Routes>
-                            <Route index element={<NagazapForm onSuccess={onAddNagazap} />} />
-                            <Route path="/*" element={<NagazapForm onSuccess={onAddNagazap} />} />
-                        </Routes>
-                    )}
-                </Box>
+                                {nagazap && (
+                                    <Box sx={{ flexDirection: "column" }}>
+                                        <ToolButton label="Informações" route="/" setShowInformations={setShowInformations} />
+                                        <ToolButton label="Mensagens" route="/messages" setShowInformations={setShowInformations} />
+                                        <ToolButton label="Criar template" route="/template-form" setShowInformations={setShowInformations} />
+                                        <ToolButton label="Enviar mensagem" route="/message-form" setShowInformations={setShowInformations} />
+                                        <ToolButton label="Forno" route="/oven" setShowInformations={setShowInformations} />
+                                        <ToolButton label="Logs" route="/logs" setShowInformations={setShowInformations} />
+                                        <ToolButton label="Lista negra" route="/blacklist" setShowInformations={setShowInformations} />
+                                        <ToolButton label="Configurações" route="/settings" setShowInformations={setShowInformations} />
+                                    </Box>
+                                )}
+                            </Box>
+                        </Title>
+                    </Paper>
+                ) : null}
+                {!isMobile || (isMobile && showInformations) ? (
+                    <Box sx={{ width: isMobile ? "100vw" : "80vw", overflowY: "auto", height: "100%" }}>
+                        {loading ? (
+                            <Box sx={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+                                <WagaLoading />
+                            </Box>
+                        ) : nagazap ? (
+                            <Routes>
+                                <Route index element={<Info nagazap={nagazap} setShowInformations={setShowInformations} />} />
+                                <Route path="/messages" element={<MessagesScreen nagazap={nagazap} />} />
+                                <Route
+                                    path="/settings"
+                                    element={
+                                        <NagazapSettings
+                                            nagazap={nagazap}
+                                            setNagazap={setNagazap}
+                                            fetchNagazaps={fetchNagazap}
+                                            setShowInformations={setShowInformations}
+                                        />
+                                    }
+                                />
+                                <Route
+                                    path="/oven"
+                                    element={<Oven nagazap={nagazap} setNagazap={setNagazap} setShowInformations={setShowInformations} />}
+                                />
+                                <Route
+                                    path="/blacklist"
+                                    element={<Blacklist nagazap={nagazap} setNagazap={setNagazap} setShowInformations={setShowInformations} />}
+                                />
+                                <Route
+                                    path="/logs"
+                                    element={<Logs nagazap={nagazap} setNagazap={setNagazap} setShowInformations={setShowInformations} />}
+                                />
+                                <Route path="/template-form" element={<TemplateForm nagazap={nagazap} setShowInformations={setShowInformations} />} />
+                                <Route
+                                    path="/message-form"
+                                    element={<MessageFormScreen nagazap={nagazap} setShowInformations={setShowInformations} />}
+                                />
+                                <Route path="/form" element={<NagazapForm onSuccess={onAddNagazap} setShowInformations={setShowInformations} />} />
+                            </Routes>
+                        ) : (
+                            <Routes>
+                                <Route index element={<NagazapForm onSuccess={onAddNagazap} setShowInformations={setShowInformations} />} />
+                                <Route path="/*" element={<NagazapForm onSuccess={onAddNagazap} setShowInformations={setShowInformations} />} />
+                            </Routes>
+                        )}
+                    </Box>
+                ) : null}
             </Box>
         </Box>
     )
