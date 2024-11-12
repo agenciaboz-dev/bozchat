17ba000eafee5306d2ab3c492636c863d523f870
ildopetrijunior <ildopetrijunior@gmail.com>
2024-11-12 14:04:47 -0300

    Update: mobile design

--- src/pages/Nagazap/Info/InfoDataContainer.tsx
+++ src/pages/Nagazap/Info/InfoDataContainer.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, IconButton, Skeleton, useMediaQuery } from "@mui/material"
+import { Box, IconButton, Skeleton, Typography, useMediaQuery } from "@mui/material"
 import { GeneralStat } from "../../../types/GeneralStat"
 import { CopyAll } from "@mui/icons-material"
 
@@ -11,17 +11,48 @@ export const InfoDataContainer: React.FC<InfoDataContainerProps> = ({ data }) =>
     const isMobile = useMediaQuery("(orientation: portrait)")
     const Icon = data.icon
     return (
-        <Box sx={{ border: "red solid 1px" }}>
-            <Box sx={{ flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
-                <Box sx={{ gap: "0.5vw", alignItems: "center" }}>
+        <Box sx={{}}>
+            <Box
+                sx={{
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    height: "100%",
+                    gap: isMobile ? "1vw" : "",
+                }}
+            >
+                <Box
+                    sx={{
+                        gap: "0.5vw",
+                        alignItems: "center",
+                    }}
+                >
                     <Icon color="secondary" sx={{ width: isMobile ? "5vw" : "1vw", height: isMobile ? "5vw" : "1vw" }} />
                     <Box sx={[{ fontSize: "0.9rem", fontWeight: "bold", color: "secondary.main" }]}>{data.title}</Box>
                 </Box>
                 {data.loading ? (
                     <Skeleton variant="rounded" animation="wave" sx={{ height: isMobile ? "9vw" : "2.21vw", width: isMobile ? "80vw" : "20vw" }} />
                 ) : (
-                    <Box sx={[{ fontSize: "1.5rem", fontWeight: "bold", color: "primary.main", gap: "0.5vw" }]}>
-                        {data.value}
+                    <Box
+                        sx={[
+                            {
+                                gap: "0.5vw",
+                                width: isMobile ? "90vw" : undefined,
+                            },
+                        ]}
+                    >
+                        <Typography
+                            sx={{
+                                fontSize: "1.5rem",
+                                fontWeight: "bold",
+                                color: "primary.main",
+                                whiteSpace: "nowrap",
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                            }}
+                        >
+                            {data.value}
+                        </Typography>
+
                         {data.copy && !!data.value && (
                             <IconButton onClick={() => navigator.clipboard.writeText(data.value!.toString())}>
                                 <CopyAll />
