8f5d17509d5b1b1fe90db73c4ce72d01fa9fc453
nandobfer <nandobfer@yahoo.com.br>
2024-11-12 11:14:08 -0300

    update: image text

--- src/pages/Nagazap/TemplateForm/TemplateHeader/TemplateHeader.tsx
+++ src/pages/Nagazap/TemplateForm/TemplateHeader/TemplateHeader.tsx
@@ -9,14 +9,11 @@ interface TemplateHeaderProps {
 }
 
 export const TemplateHeader: React.FC<TemplateHeaderProps> = ({ component }) => {
+    const url = component.file ? URL.createObjectURL(component.file) : undefined
     return component.format === "TEXT" ? (
         <TextHeader component={component} />
     ) : (
-        <Avatar
-            variant="rounded"
-            src={component.file ? URL.createObjectURL(component.file) : undefined}
-            sx={{ width: "100%", height: "auto", aspectRatio: 1, bgcolor: "background.default" }}
-        >
+        <Avatar variant="rounded" src={url} sx={{ width: "100%", height: url ? "auto" : "20vw", maxHeight: "20vw", bgcolor: "background.default" }}>
             <CloudUpload color="primary" sx={{ width: "30%", height: "auto" }} />
         </Avatar>
     )
