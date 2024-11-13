4fb36e8da1f95790ade4d12f47b6483a5f437f72
ildopetrijunior <ildopetrijunior@gmail.com>
2024-11-13 19:02:58 -0300

    Update: layout info on mobile

--- src/pages/Nagazap/Info/Info.tsx
+++ src/pages/Nagazap/Info/Info.tsx
@@ -125,12 +125,19 @@ export const Info: React.FC<InfoProps> = ({ nagazap, setShowInformations }) => {
                         ))}
                     </Box>
                 </Grid>
-                <Grid item xs={2}>
-                    <GeneralStatistics nagazap={nagazap} />
-                </Grid>
+                {isMobile ? (
+                    <Grid item xs={2}>
+                        <GeneralStatistics nagazap={nagazap} />
+                    </Grid>
+                ) : null}
                 <Grid item xs={2}>
                     <MessagesChart messages={nagazap.sentMessages} />
                 </Grid>
+                {!isMobile ? (
+                    <Grid item xs={2}>
+                        <GeneralStatistics nagazap={nagazap} />
+                    </Grid>
+                ) : null}
                 <Grid item xs={2}>
                     <BlacklistChart blacklist={nagazap.blacklist} />
                 </Grid>
