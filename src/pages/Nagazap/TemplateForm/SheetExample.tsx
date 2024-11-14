8d5aee14b8441dbf159da5bcc67359b6b256d01c
nandobfer <nandobfer@yahoo.com.br>
2024-11-14 09:52:03 -0300

    update: sheets example

--- src/pages/Nagazap/TemplateForm/SheetExample.tsx
+++ src/pages/Nagazap/TemplateForm/SheetExample.tsx
@@ -10,7 +10,9 @@ const SheetColumn: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 const SheetLine: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <Box sx={{ padding: "5px", border: "solid ", borderWidth: "1px 0" }}>
-            <Typography color="secondary.main">{children}</Typography>
+            <Typography color="secondary.main" fontSize={"0.85rem"}>
+                {children}
+            </Typography>
         </Box>
     )
 }
@@ -26,16 +28,16 @@ export const SheetExample: React.FC<SheetExampleProps> = ({}) => {
             }}
         >
             <SheetColumn>
-                <SheetLine>Nome:</SheetLine>
-                <SheetLine>João</SheetLine>
-                <SheetLine>Maria</SheetLine>
-                <SheetLine>José</SheetLine>
+                <SheetLine>outras colunas</SheetLine>
+                <SheetLine>...</SheetLine>
+                <SheetLine>...</SheetLine>
+                <SheetLine>...</SheetLine>
             </SheetColumn>
             <SheetColumn>
-                <SheetLine>Telefone:</SheetLine>
-                <SheetLine>(41) 00000-0000</SheetLine>
-                <SheetLine>(11) 11111-1111</SheetLine>
-                <SheetLine>(91) 22222-2222</SheetLine>
+                <SheetLine>Telefone</SheetLine>
+                <SheetLine>(41) 0 0000-0000</SheetLine>
+                <SheetLine>41 00000 0000</SheetLine>
+                <SheetLine>41000000000</SheetLine>
             </SheetColumn>
         </Box>
     )
