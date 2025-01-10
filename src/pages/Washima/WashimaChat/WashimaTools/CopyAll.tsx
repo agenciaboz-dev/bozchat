20568e174170a6f458d5c5ed93f33fc5f0d10281
nandobfer <nandobfer@yahoo.com.br>
2025-01-10 14:18:22 -0300

    feat: added media recognition on copied text

--- src/pages/Washima/WashimaChat/WashimaTools/CopyAll.tsx
+++ src/pages/Washima/WashimaChat/WashimaTools/CopyAll.tsx
@@ -4,6 +4,7 @@ import { api } from "../../../../api"
 import { WashimaMessage } from "../../../../types/server/class/Washima/WashimaMessage"
 import { Chat } from "../../../../types/Chat"
 import { Check, CheckCircle, CopyAll } from "@mui/icons-material"
+import { WashimaMedia } from "../../../../types/server/class/Washima/Washima"
 
 interface CopyAllProps {
     chat: Chat
@@ -13,6 +14,11 @@ export const CopyAllButton: React.FC<CopyAllProps> = ({ chat }) => {
     const [loading, setLoading] = useState(false)
     const [copied, setCopied] = useState(false)
 
+    const fetchMetadata = async (washima_id: string, message_id: string) => {
+        const response = await api.get("/washima/media-metadata", { params: { washima_id, message_id } })
+        return response.data as WashimaMedia
+    }
+
     const copyAllMessages = async () => {
         if (loading) return
         const texts: string[] = []
@@ -21,14 +27,22 @@ export const CopyAllButton: React.FC<CopyAllProps> = ({ chat }) => {
         try {
             const response = await api.get("/washima/tools/copy-chat", { params: { chat_id: chat.id._serialized } })
             const messages = response.data as WashimaMessage[]
-            messages
-                .sort((a, b) => Number(a.timestamp) - Number(b.timestamp))
-                .forEach((message) => {
-                    const text = `${new Date(Number(message.timestamp * 1000)).toLocaleString("pt-br")} - ${
-                        message.fromMe ? "Eu:" : (message.author || chat.name) + ":"
-                    } ${message.body}`
-                    texts.push(text)
-                })
+
+            await Promise.all(
+                messages
+                    .sort((a, b) => Number(a.timestamp) - Number(b.timestamp))
+                    .map(async (message) => {
+                        const media = message.hasMedia ? await fetchMetadata(message.washima_id, message.sid) : null
+                        const text = `${new Date(Number(message.timestamp * 1000)).toLocaleString("pt-br")} - ${
+                            message.fromMe ? "Eu:" : (message.author || chat.name) + ":"
+                        } ${
+                            media
+                                ? `[[ARQUIVO]] \nnome: ${media.filename}\ntamanho: ${media.size}\nmensagem: ${message.body}\n[[FIM DO ARQUIVO]]`
+                                : message.body
+                        }`
+                        texts.push(text)
+                    })
+            )
 
             navigator.clipboard.writeText(texts.join("\n"))
             setCopied(true)
