34ea796a90b8a6f2b1b88f91ea12e3ad9542b43e
ispadilha <israel.padilha@gmail.com>
2025-04-03 10:22:17 -0300

    style: logs subroute in broadcast for mobile

--- src/pages/Nagazap/Logs/LogsList.tsx
+++ src/pages/Nagazap/Logs/LogsList.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Box, Grid, Paper } from "@mui/material"
+import { Box, Divider, Grid, Paper, useMediaQuery } from "@mui/material"
 import { LogContainer } from "./LogContainer"
 import { FailedMessageLog, SentMessageLog } from "../../../types/server/Meta/WhatsappBusiness/Logs"
 import { FailedLogContainer } from "./FailedLogContainer"
@@ -11,17 +11,24 @@ interface LogsListProps {
 }
 
 export const LogsList: React.FC<LogsListProps> = ({ list, type }) => {
+    const isMobile = useMediaQuery("(orientation: portrait)")
     return (
-        <Grid item xs={1}>
-            <Paper sx={{ flexDirection: "column", padding: "1vw", gap: "1vw" }}>
+        <Grid item xs={1} sx={{ paddingBottom: isMobile ? "5vw" : "2vw" }}>
+            <Paper sx={{ flexDirection: "column", padding: isMobile ? "5vw" : "1vw", gap: isMobile ? "3vw" : "0.5vw" }}>
                 <Title2 name={type == "success" ? "Enviadas" : "Falhas"} />
                 {list
                     .sort((a, b) => Number(b.timestamp) - Number(a.timestamp))
-                    .map((item) =>
+                    .map((item, index, arr) =>
                         type == "success" ? (
-                            <LogContainer key={item.timestamp} log={item as SentMessageLog} />
+                            <>
+                                <LogContainer key={item.timestamp} log={item as SentMessageLog} />
+                                {index !== arr.length - 1 && <Divider />}
+                            </>
                         ) : (
-                            <FailedLogContainer key={item.timestamp} log={item as FailedMessageLog} />
+                            <>
+                                <FailedLogContainer key={item.timestamp} log={item as FailedMessageLog} />
+                                {index !== arr.length - 1 && <Divider />}
+                            </>
                         )
                     )}
             </Paper>
--- src/version.ts
+++ src/version.ts
@@ -1,1 +1,1 @@
-export const version = "7.16.6"
+export const version = "7.16.7"
