48736c98642415dde5e74cce660a886252827b3a
nandobfer <nandobfer@yahoo.com.br>
2025-04-17 18:12:17 -0300

    backend

--- src/api/backend.ts
+++ src/api/backend.ts
@@ -1,3 +1,3 @@
-// export const url = "s://apichat.boz.app.br"
-export const url = "://localhost:8105"
+export const url = "s://apichat.boz.app.br"
+// export const url = "://localhost:8105"
 // export const url = "://192.168.15.23:8105"
