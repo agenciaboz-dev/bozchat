da003a4bb056018d45e4d7e3b87ad861d4740299
nandobfer <nandobfer@yahoo.com.br>
2025-04-17 18:07:45 -0300

    chore: remade washima sidebar

--- src/pages/Washima/WashimaSidebar.tsx
+++ src/pages/Washima/WashimaSidebar.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { Box, CircularProgress, MenuItem, Paper, Typography, useMediaQuery } from "@mui/material"
 import { Washima } from "../../types/server/class/Washima/Washima"
-import { QrCodeScanner } from "@mui/icons-material"
+import { Circle, QrCodeScanner } from "@mui/icons-material"
+import { phoneMask } from "../../tools/masks"
 
 export interface WashimaSidebarProps {
     washimas: Washima[]
@@ -10,14 +11,17 @@ export interface WashimaSidebarProps {
 }
 
 export const WashimaSidebar: React.FC<WashimaSidebarProps> = (props) => {
-    const isMobile = useMediaQuery('(orientation: portrait)')
+    const isMobile = useMediaQuery("(orientation: portrait)")
     return (
         <Paper
             sx={{
-                flex: isMobile ? 1 : 0.1,
+                flex: isMobile ? 1 : 0.16,
                 flexDirection: "column",
                 alignItems: "center",
-                padding: isMobile ? "5vw" : "2vw",
+                padding: isMobile ? "5vw" : "0vw",
+                height: "75vh",
+                overflowY: "auto",
+                overflowX: "hidden",
             }}
         >
             {props.washimas
@@ -29,28 +33,39 @@ export const WashimaSidebar: React.FC<WashimaSidebarProps> = (props) => {
                             key={item.id}
                             sx={{
                                 width: 1,
-                                margin: "0 -2vw",
                                 flexShrink: 0,
-                                outline: active ? "1px solid" : "",
                                 borderRadius: "0.3vw",
                                 justifyContent: "space-between",
+                                whiteSpace: "normal",
+                                padding: 0,
                             }}
                             onClick={() => {
                                 props.onClick(item)
                             }}
+                            disabled={item.status !== "ready"}
                         >
-                            <Typography
-                                style={{
-                                    maxWidth: "calc(100% - 1.5vw)",
-                                    whiteSpace: "nowrap",
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis",
-                                }}
-                            >
-                                {item.name}
-                            </Typography>
-                            {item.status === 'loading' && <CircularProgress size="1rem" color="warning" />}
-                            {item.status === 'qrcode' && <QrCodeScanner color="warning" sx={{ width: isMobile ? "7vw" : "1.3vw", height: isMobile ? "7vw" : "1.3vw" }} />}
+                            <Paper elevation={active ? 2 : 0} sx={{ padding: "1vw", flex: 1, flexDirection: "column" }}>
+                                <Typography
+                                    sx={{
+                                        maxWidth: "calc(100% - 0vw)",
+                                        whiteSpace: "nowrap",
+                                        overflow: "hidden",
+                                        textOverflow: "ellipsis",
+                                    }}
+                                >
+                                    {item.name}
+                                </Typography>
+                                <Box sx={{ justifyContent: "space-between", alignItems: "center" }}>
+                                    <Typography sx={{ color: "secondary.main", fontSize: "0.8rem" }}>{phoneMask.format(item.number)}</Typography>
+                                    {item.status === "ready" && (
+                                        <Paper sx={{ borderRadius: "100%" }}>
+                                            <Circle fontSize="small" color="success" />
+                                        </Paper>
+                                    )}
+                                    {item.status === "loading" && <CircularProgress size="1.2rem" color="warning" />}
+                                    {item.status === "qrcode" && <QrCodeScanner color="secondary" fontSize="small" />}
+                                </Box>
+                            </Paper>
                         </MenuItem>
                     )
                 })}
