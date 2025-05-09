6969134d6f1102ba190d1491c795fb26a4ea367e
nandobfer <nandobfer@yahoo.com.br>
2025-05-09 15:16:38 -0300

    feat: reading qrcode from the no-chat screen list

--- src/pages/Washima/WashimaSidebar.tsx
+++ src/pages/Washima/WashimaSidebar.tsx
@@ -1,8 +1,9 @@
 import React from "react"
-import { Box, CircularProgress, MenuItem, Paper, Typography, useMediaQuery } from "@mui/material"
+import { Box, CircularProgress, MenuItem, Paper, Tooltip, Typography, useMediaQuery } from "@mui/material"
 import { Washima } from "../../types/server/class/Washima/Washima"
 import { Circle, QrCodeScanner } from "@mui/icons-material"
 import { phoneMask } from "../../tools/masks"
+import { QRCode } from "react-qrcode-logo"
 
 export interface WashimaSidebarProps {
     washimas: Washima[]
@@ -28,46 +29,50 @@ export const WashimaSidebar: React.FC<WashimaSidebarProps> = (props) => {
                 .map((item) => {
                     const active = props.currentWashima?.id === item.id
                     return (
-                        <MenuItem
-                            key={item.id}
-                            sx={{
-                                width: "15vw",
-                                flexShrink: 0,
-                                borderRadius: "0.3vw",
-                                justifyContent: "space-between",
-                                whiteSpace: "normal",
-                                padding: 0,
-                                backgroundColor: active ? "action.hover" : undefined,
-                            }}
-                            onClick={() => {
-                                props.onClick(item)
-                            }}
-                            disabled={item.status !== "ready"}
-                        >
-                            <Paper elevation={active ? 2 : 1} sx={{ padding: "1vw", flex: 1, flexDirection: "column" }}>
-                                <Typography
-                                    sx={{
-                                        maxWidth: "13vw",
-                                        whiteSpace: "nowrap",
-                                        overflow: "hidden",
-                                        textOverflow: "ellipsis",
-                                        fontSize: "0.9rem",
-                                    }}
-                                >
-                                    {item.name}
-                                </Typography>
-                                <Box sx={{ justifyContent: "space-between", alignItems: "center" }}>
-                                    <Typography sx={{ color: "text.secondary", fontSize: "0.8rem" }}>{phoneMask.format(item.number)}</Typography>
-                                    {item.status === "ready" && (
-                                        <Paper sx={{ borderRadius: "100%" }}>
-                                            <Circle fontSize="small" color="success" />
-                                        </Paper>
-                                    )}
-                                    {item.status === "loading" && <CircularProgress size="1.2rem" color="warning" />}
-                                    {item.status === "qrcode" && <QrCodeScanner color="action" fontSize="small" />}
-                                </Box>
-                            </Paper>
-                        </MenuItem>
+                        <Tooltip title={item.status === "qrcode" ? <QRCode value={item.qrcode} size={250} /> : null} arrow placement="right">
+                            <MenuItem
+                                key={item.id}
+                                sx={{
+                                    width: "15vw",
+                                    flexShrink: 0,
+                                    borderRadius: "0.3vw",
+                                    justifyContent: "space-between",
+                                    whiteSpace: "normal",
+                                    padding: 0,
+                                    backgroundColor: active ? "action.hover" : undefined,
+                                }}
+                                onClick={() => {
+                                    if (item.status !== "ready") return
+                                    props.onClick(item)
+                                }}
+                                // disabled={item.status !== "ready"}
+                                disableRipple={item.status !== "ready"}
+                            >
+                                <Paper elevation={active ? 2 : 1} sx={{ padding: "1vw", flex: 1, flexDirection: "column" }}>
+                                    <Typography
+                                        sx={{
+                                            maxWidth: "13vw",
+                                            whiteSpace: "nowrap",
+                                            overflow: "hidden",
+                                            textOverflow: "ellipsis",
+                                            fontSize: "0.9rem",
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Typography>
+                                    <Box sx={{ justifyContent: "space-between", alignItems: "center" }}>
+                                        <Typography sx={{ color: "text.secondary", fontSize: "0.8rem" }}>{phoneMask.format(item.number)}</Typography>
+                                        {item.status === "ready" && (
+                                            <Paper sx={{ borderRadius: "100%" }}>
+                                                <Circle fontSize="small" color="success" />
+                                            </Paper>
+                                        )}
+                                        {item.status === "loading" && <CircularProgress size="1.2rem" color="warning" />}
+                                        {item.status === "qrcode" && <QrCodeScanner color="action" fontSize="small" />}
+                                    </Box>
+                                </Paper>
+                            </MenuItem>
+                        </Tooltip>
                     )
                 })}
         </Paper>
